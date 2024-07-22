083e6519b3c4241db22112a2b23e30f990a6ea5e
Shikuku39 <enshikuku39@gmail.com>
2024-07-22 12:26:18 +0300

    chore: Add initial project files and structure

---
+++ src/app/app.routes.ts
@@ -0,0 +1,14 @@
+import { Routes } from '@angular/router';
+import { HomeComponent } from './home/home.component';
+import { LoginComponent } from './login/login.component';
+import { RegisterComponent } from './register/register.component';
+import { BlogListComponent } from './blog-list/blog-list.component';
+
+
+export const routes: Routes = [
+    { path: '', redirectTo: '/home', pathMatch: 'full' },
+    { path: 'home', component: HomeComponent },
+    { path: 'login', component: LoginComponent},
+    { path: 'register', component: RegisterComponent},
+    { path: 'blog-list', component: BlogListComponent}
+];
---
+++ src/app/blog-list/blog-list.component.ts
@@ -0,0 +1,27 @@
+import { Component, OnInit } from '@angular/core';
+import { BlogService } from '../blog.service';
+import { RouterLink } from '@angular/router';
+// import { CommonModule } from '@angular/common';
+
+@Component({
+  selector: 'app-blog-list',
+  standalone: true,
+  imports: [RouterLink],
+  templateUrl: './blog-list.component.html',
+  styleUrl: './blog-list.component.css'
+})
+export class BlogListComponent implements OnInit {
+  posts: any[] = [];
+
+  constructor(private blogService: BlogService) { }
+
+  ngOnInit(): void {
+    this.loadPosts();
+  }
+
+  loadPosts(): void {
+    this.blogService.getPosts().subscribe(data => {
+      this.posts = data;
+    });
+  }
+}
