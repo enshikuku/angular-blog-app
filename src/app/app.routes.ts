ffb3d90a23ebeb22f8f82e518d92490b985a8109
Shikuku39 <enshikuku39@gmail.com>
2024-07-22 13:30:59 +0300

    feat: Add blog form component and routes for creating and updating blog posts

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -4,6 +4,7 @@ import { LoginComponent } from './login/login.component';
 import { RegisterComponent } from './register/register.component';
 import { BlogListComponent } from './blog-list/blog-list.component';
 import { BlogDetailComponent } from './blog-detail/blog-detail.component';
+import { BlogFormComponent } from './blog-form/blog-form.component';
 
 
 export const routes: Routes = [
@@ -11,13 +12,8 @@ export const routes: Routes = [
     { path: 'home', component: HomeComponent },
     { path: 'login', component: LoginComponent},
     { path: 'register', component: RegisterComponent},
-    { 
-        path: 
-            'blog-list', 
-        component: 
-            BlogListComponent,
-    },{
-        path: 'blog-list/:id',
-        component: BlogDetailComponent
-    }
+    { path: 'blog-list', component: BlogListComponent,},
+    { path: 'blog-list/:id',component: BlogDetailComponent},
+    { path: 'blog-form', component: BlogFormComponent,},
+    { path: 'blog-form/:id',component: BlogFormComponent}
 ];
